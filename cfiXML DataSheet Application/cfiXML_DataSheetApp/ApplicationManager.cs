92f49db30125c72613da7b82a589c1a89920c2bd
caueclasen <caueclasen@gmail.com>
2014-08-07 15:40:46 -0300

    Revert "sadfc"
    This reverts commit 05db7c66e26d9723c3099992e68ad6f70b7802e2.

--- cfiXML DataSheet Application/cfiXML_DataSheetApp/ApplicationManager.cs
+++ cfiXML DataSheet Application/cfiXML_DataSheetApp/ApplicationManager.cs
@@ -17,7 +17,7 @@ namespace cfiXML_DataSheetApp
         private IXLS_Handler _xlsHandler;
 
         /// <summary>
-        /// Construtorrrrrrrr
+        /// Construtor
         /// </summary>
         public ApplicationManager()
         {
